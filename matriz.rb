6f331d0822267afd8103be7f04729e5ffce99b83
Jose Angel Concepcion <alu0100697414@ull.edu.es>
2013-10-08 19:18:08 +0100

    Suma realizada

---
+++ matriz.rb
@@ -0,0 +1,24 @@
+# require 'matrix'
+# 
+#  m1 = Matrix[[1,2,3],[4,5,6],[7,8,9]] # Primera matriz
+#  m2 = Matrix[[1,2,3],[4,5,6],[7,8,9]] # Segunda matriz
+# 
+# puts m1+m2 # Suma de la matriz
+# puts m1*m2  # Multiplicación de la matriz*/
+
+
+  
+
+m1 = [[1,2,3],[4,5,6],[7,8,9]] # Primera matriz
+m2 = [[1,2,3],[4,5,6],[7,8,9]] # Segunda matriz
+m3 = [[0,0,0],[0,0,0],[0,0,0]] # matriz resultado
+
+#SUMA
+
+  for i in (0..2)
+    for j in (0..2)
+      m3[i][j] = m1[i][j] + m2[i][j]
+    end
+  end
+  print m3
+
